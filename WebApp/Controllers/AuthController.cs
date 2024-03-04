970fe618f96c6254ac166dd61ae83aaa941b4839
Tim <tim.martensson92@gmail.com>
2024-03-04 09:08:58 +0100

    auth init

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
