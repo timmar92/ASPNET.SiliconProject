2b903f6a69c03ae311123c6d8a446ac63ebf0ec9
Tim <tim.martensson92@gmail.com>
2024-03-04 09:20:55 +0100

    auth init

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -4,8 +4,37 @@ namespace WebApp.Controllers;
 
 public class AuthController : Controller
 {
-    public IActionResult Index()
+    [HttpGet]
+    [Route("/signup")]
+    public IActionResult SignUp()
     {
         return View();
     }
+
+
+
+    //[HttpPost]
+    //[Route("/signup")]
+    //public IActionResult SignUp()
+    //{
+    //    return View();
+    //}
+
+
+    [HttpGet]
+    [Route("/signin")]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+
+    //[HttpPost]
+    //[Route("/signin")]
+    //public async Task<IActionResult> SignIn()
+    //{
+    //    return View();
+    //}
+
+
 }
