2698694c33221fb9ea33d56c431b0261c7e6b492
Tim <tim.martensson92@gmail.com>
2024-03-05 10:05:05 +0100

    account init

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
