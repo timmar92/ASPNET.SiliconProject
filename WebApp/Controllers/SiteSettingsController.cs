094c8f0eabcd32241265e3485344b95c594de437
Tim <tim.martensson92@gmail.com>
2024-04-03 18:30:12 +0200

    dark mode done, needs testing

---
+++ WebApp/Controllers/SiteSettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class SiteSettingsController : Controller
+{
+    public IActionResult ChangeTheme(string mode)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddMonths(2)
+        };
+        Response.Cookies.Append("ThemeMode", mode, option);
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult CookieConsent()
+    {
+
+    }
+}
--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    let switchButton = document.querySelector('#switch-mode')
+
+    switchButton.addEventListener('change', function () {
+        let theme = this.checked ? 'dark' : 'light'
+
+        fetch(`/sitesettings/changetheme?mode=${theme}`)
+            .then(response => {
+                if (response.ok) {
+                    window.location.reload()
+                }
+                else {
+                    console.log('Error')
+                }
+            });
+    });
+});
