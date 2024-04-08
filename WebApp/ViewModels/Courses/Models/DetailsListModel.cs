ee0013f66146f8de768870a0b9f9d75ccebe206e
Tim <tim.martensson92@gmail.com>
2024-04-08 12:46:46 +0200

    fixed details

--- WebApp/ViewModels/Courses/Models/DetailsListModel.cs
+++ WebApp/ViewModels/Courses/Models/DetailsListModel.cs
@@ -14,6 +14,17 @@
         public string? Detail_9 { get; set; }
         public string? Detail_10 { get; set; }
 
+        public string? Title_1 { get; set; }
+        public string? Title_2 { get; set; }
+        public string? Title_3 { get; set; }
+        public string? Title_4 { get; set; }
+        public string? Title_5 { get; set; }
+        public string? Title_6 { get; set; }
+        public string? Title_7 { get; set; }
+        public string? Title_8 { get; set; }
+        public string? Title_9 { get; set; }
+        public string? Title_10 { get; set; }
+
         public int CourseEntityId { get; set; }
     }
 }
